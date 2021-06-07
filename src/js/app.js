97c07d4475a95c4e18673008efb23c907bbbea8d
Akhil Suden <59475385+Akhil-Suden@users.noreply.github.com>
2021-06-07 18:05:29 +0530

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -1,74 +1,60 @@
 App = {
   web3Provider: null,
   contracts: {},
-
-  init: async function() {
-    return await App.initWeb3();
-  },
-
+  account: '0x0',
   initWeb3: async function() {
-    if (window.ethereum) {
-      App.web3Provider = window.ethereum;
-      try {
-        // Request account access
-        await window.ethereum.enable();
-      } catch (error) {
-        // User denied account access...
-        console.error("User denied account access")
-      }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      App.web3Provider = window.web3.currentProvider;
-    }
-    // If no injected web3 instance is detected, fall back to Ganache
-    else {
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-    }
-    web3 = new Web3(App.web3Provider);
+        if (window.ethereum) {
+          App.web3Provider = window.ethereum;
+          try {
+            await window.ethereum.enable();            // Request account access
+            console.log("connected to web3Provider");
+          } catch (error) {
+            console.error("User denied account access")
+          }
+        }
+        else if (window.web3) {             // Legacy dapp browsers...
+          App.web3Provider = window.web3.currentProvider;
+        }
+        else {
+          App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');  //Ganache
+        }
+        web3 = new Web3(App.web3Provider);
 
-    return App.initContract();
+        return App.initContract();
   },
 
 initContract: function() {
-  $.getJSON('Vaccine.json', function(data) {
-  // Get the necessary contract artifact file and instantiate it with @truffle/contract
-  var  VaccineArtifact = data;
-  App.contracts.Vaccine = TruffleContract(VaccineArtifact);
-
-  // Set the provider for our contract
-  App.contracts.Vaccine.setProvider(App.web3Provider);
-});
+  $.getJSON("Vaccine.json", function(data) {
+    var VaccineArtifact = data;
+    App.contracts.Vaccine = TruffleContract(VaccineArtifact);
+    App.contracts.Vaccine.setProvider(App.web3Provider);
+  });
 
   return App.bindEvents();
   },
 
   bindEvents: function() {
-  $(document).on('click','#submit', App.handleRecord);
+  $( "form" ).submit(App.handleRecord);
   },
 
   handleRecord: function(event) {
-  var batch= $("#batch").val();
-  var temp= parseInt($("#temp").val());
-  var recordInstance;
-
-  web3.eth.getAccounts(function(error, accounts) {
-    if (error) {
-      console.log(error);
-    }
-    var account = accounts[0];
-
-    App.contracts.Vaccine.deployed().then(function(instance) {
-      adoptionInstance = instance;
-      return recordInstance.takevalue(batch, temp, {from: account});
-      });
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      } else {
+        App.account = accounts[0];
+      }
     });
-  }
+    App.contracts.Vaccine.deployed().then(function(instance) {
+      VaccineInstance = instance;
+      return VaccineInstance.initialise();
+    });
 
+  }
 };
 
 $(function() {
-  $(window).load(function() {
-    App.init();
+   $(window).load(function() {
+    App.initWeb3();
   });
 });
